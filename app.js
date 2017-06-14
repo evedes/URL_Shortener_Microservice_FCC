9e5b186803f6eeac04a105610ebc438d5f6c1e5c
Eduardo Vedes <eduardo.vedes@gmail.com>
2017-06-15 00:24:37 +0100

    update

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const port = process.env.PORT || 8080
 
 app.use('/', express.static(path.join(__dirname,'public')))
 
+app.get('/new/:url2Short(*)', (req,res)=>{
+   
+    let url = req.params.url2Short
+    let regEx1 = /http?:\/\//
+    let parsedUrl=url.replace(regEx1,'')
+    let shortUrl = Math.floor(Math.random()*10000)
+    
+    res.json({url: parsedUrl, shorturl: shortUrl})
+    
+})
+
 app.listen(port, ()=>{
-    console.log('Server listening on port: ' + 8080)
+    console.log('Server listening on port: ' + port)
 })
