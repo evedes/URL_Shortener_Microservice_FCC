73075f66da3c5771033753697db79a048bad3062
Eduardo Vedes <eduardo.vedes@gmail.com>
2017-06-19 13:57:53 +0100

    connection.js defines the Schema and Models. Db connection went to app.js file

--- db/connection.js
+++ db/connection.js
@@ -1,20 +1,12 @@
 const mongoose = require('mongoose')
 
-
-mongoose.connect('mongodb://' + process.env.DB_USER +':' + process.env.DB_PASS + process.env.DB_HOST)
-var db = mongoose.connection
-db.on('error',console.error.bind(console,'connection error:'))
-db.once('open', ()=>{
-    console.log('\nHey guys! We\'re connected!\n')
-})
-
 // Schema Definitions
 var urlSchema = mongoose.Schema({
     Url: String,
     ShortUrl: String
 })
 
-//Define module
+//Define model
 var urlToShorten = mongoose.model('urlToShorten', urlSchema,'urlCollection')
 
 module.exports = urlToShorten
