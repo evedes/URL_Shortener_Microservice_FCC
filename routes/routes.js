25f747bb4c33162c07f7ffe6fe0fa4ffff61b544
Eduardo Vedes <eduardo.vedes@gmail.com>
2017-06-21 22:23:32 +0100

    took off functions
    took off functions to lib/lib.js file. Changed the way routes are done according to this movement

--- routes/routes.js
+++ routes/routes.js
@@ -2,68 +2,21 @@ const express = require('express')
 const path = require('path')
 var router = express.Router()
 var urlToShorten = require('../db/connection.js')
-
+var lib = require('../lib/lib.js')
 
 //ATTRIB STATIC PATH TO PUBLIC FOLDER
 router.use('/', express.static(path.join(__dirname,'../public')))
 
-
+//ATTRIB /s PATH TO SELF REDIRECTION
 router.get('/s/', (req,res)=>{
     res.redirect(301,'https://dust-fountain.glitch.me/')
 })
 
-// GET DATA FROM URL
-router.get('/new/:url2Short(*)', (req,res)=>{
-    
-    let url = req.params.url2Short
-    let regEx1 = /^https?:\/\//
-    let test = regEx1.test(url)
-        
-        if (test===true) {
-
-            urlToShorten.findOne({Url: url}, (err,count)=>{
-                if(err) return res.send('Error reading database')
-                if(count===null) {
-
-                    let shortUrl = Math.floor(Math.random()*10000).toString()
-                    let data = new urlToShorten({Url: url, ShortUrl: shortUrl})
-              
-                    data.save((err,data)=>{
-                        if(err) return console.error(err)
-                    })
-    
-                    res.json({Url: data.Url, ShortUrl: data.ShortUrl})
-                }
-                else {
-                    
-                    res.json({Message: 'Url already exists!', Url: count.Url, ShortUrl: count.ShortUrl})
-                }
-
-            })                                
-            
-        }
-        else {
-            res.json({Message: 'Wrong url format, make sure you have a valid protocol and a real site.'})
-        }
-   
-})
+// GET DATA AND CREATE A NEW URL OR IF URL EXISTS SEND INFO ABOUT SHORTURL CREATED
+router.route('/new/:url2Short(*)').get(lib.newUrl)
 
 // USE SHORTENED URL
-router.get('/s/:shortenedUrl', (req,res)=>{
-    
-    var shortenedUrl = req.params.shortenedUrl;
-    
-    urlToShorten.findOne({ShortUrl:shortenedUrl}, (err,count)=>{
-        if(err) return res.send('Error reading database')
-        
-        if(count===null) {
-            res.json({Message: 'Short URL doesn\'t exists!'})
-        
-        } else { 
-            res.redirect(301,count.Url)
-        } 
-    })
-})
+router.route('/s/:shortenedUrl').get(lib.redirectShortUrl)
 
 
 module.exports = router
